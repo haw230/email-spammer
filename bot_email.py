483acd64824c58dd850df38ad5a88c80d5f99e62
Han Wu <hancheng.wu22@gmail.com>
2018-05-20 23:53:55 -0600

    Add files via upload

---
+++ bot_email.py
@@ -0,0 +1,43 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import requests
+from time import sleep
+gmail = 'x@gmail.com'
+pw = 'password'
+mail = smtplib.SMTP('smtp.gmail.com', 587)
+mail.ehlo()
+mail.starttls()
+mail.login(gmail, pw)
+alpha = 'stuvwxyz'
+
+start = 0
+for letter in alpha:
+    count = 0
+    temp = requests.get("http://universities.hipolabs.com/search?country=United%20States&name=" + letter).json()
+    for i in range(start, len(temp)):
+        dom = "admissions@" + temp[i]['domains'][0]
+        bodyText = "Dear " + temp[i]['name'] + ", \n\nI am a high school senior in Canada interested in heading south for college. I am an academically-driven student (top 3% of largest school in the province and 99th percentile SAT) looking for a rigorous education, and " + temp[i]['name'] + " seems like the perfect match. However, being in Canada, it is difficult to find both time and money to travel and take a look at the open house. But if there is anything (swag, brochures, shirts etc) left over, I would love it. My mailing address is 9407 176th Street NW Edmonton, Alberta, Canada. Postal code is T5T 3W2."
+        bodyText = bodyText + "\n\n Thank you in advance! \n "
+        msg = MIMEText(bodyText)
+        msg['Subject'] = 'Prospective Canadian High School Student'
+        msg['From'] = gmail
+        msg['Reply-To'] = gmail
+        msg['To'] = dom
+        mail.sendmail(gmail, dom, msg.as_string())
+        count += 1
+        if(count == 35):
+            count = 0
+            mail.quit()
+            sleep(700)
+            mail = smtplib.SMTP('smtp.gmail.com', 587)
+            mail.ehlo()
+            mail.starttls()
+            mail.login(gmail, pw)
+        print("Sent")
+        sleep(1)
+        f = open('log.txt','w')
+        f.write(letter + str(i) + "\n")
+        f.close()
+    start = 0
+
