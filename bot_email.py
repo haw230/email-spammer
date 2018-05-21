a59a6241e254c2012cd52b70b5201440d4a8382f
Han Wu <hancheng.wu22@gmail.com>
2018-05-20 23:55:34 -0600

    Update bot_email.py

--- bot_email.py
+++ bot_email.py
@@ -17,7 +17,13 @@ for letter in alpha:
     temp = requests.get("http://universities.hipolabs.com/search?country=United%20States&name=" + letter).json()
     for i in range(start, len(temp)):
         dom = "admissions@" + temp[i]['domains'][0]
-        bodyText = "Dear " + temp[i]['name'] + ", \n\nI am a high school senior in Canada interested in heading south for college. I am an academically-driven student (top 3% of largest school in the province and 99th percentile SAT) looking for a rigorous education, and " + temp[i]['name'] + " seems like the perfect match. However, being in Canada, it is difficult to find both time and money to travel and take a look at the open house. But if there is anything (swag, brochures, shirts etc) left over, I would love it. My mailing address is 9407 176th Street NW Edmonton, Alberta, Canada. Postal code is T5T 3W2."
+        bodyText = "Dear " + temp[i]['name'] + """
+        , \n\nI am a high school senior in Canada interested in heading south for college. 
+        I am an academically-driven student (top 3% of largest school in the province and 99th percentile SAT) looking for a rigorous 
+        education, and " + temp[i]['name'] + " seems like the perfect match. However, being in Canada, it is difficult to find both time 
+        and money to travel and take a look at the open house. But if there is anything (swag, brochures, shirts etc) left over, I would 
+        love it. My mailing address is X. Postal code is Y.
+        """
         bodyText = bodyText + "\n\n Thank you in advance! \n "
         msg = MIMEText(bodyText)
         msg['Subject'] = 'Prospective Canadian High School Student'
